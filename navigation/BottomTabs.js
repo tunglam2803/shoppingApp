c061d805652754be06a7c8c26425dcc83eb3cac8
unknown <lamcityhunter2803@gmail.com>
2026-04-07 09:31:35 +0700

    update project

--- navigation/BottomTabs.js
+++ navigation/BottomTabs.js
@@ -1,5 +1,7 @@
 // navigation/BottomTabs.js
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import CartScreen from "../screens/CartScreen";
 import ExploreScreen from "../screens/ExploreScreen";
 import FavouriteScreen from "../screens/FavouriteScreen";
@@ -9,7 +11,36 @@ const Tab = createBottomTabNavigator();
 
 export default function BottomTabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+
+        tabBarActiveTintColor: "#53B175",
+        tabBarInactiveTintColor: "#000",
+
+        tabBarStyle: {
+          height: 65,
+          paddingBottom: 8,
+          paddingTop: 5,
+        },
+
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Explore") iconName = "search-outline";
+          if (route.name === "Filter") iconName = "options-outline";
+          if (route.name === "Cart") iconName = "cart-outline";
+          if (route.name === "Favourite") iconName = "heart-outline";
+
+          return <Ionicons name={iconName} size={22} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name="Explore" component={ExploreScreen} />
       <Tab.Screen name="Filter" component={FilterScreen} />
       <Tab.Screen name="Cart" component={CartScreen} />
